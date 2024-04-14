02c193664c944decaf8b44d600f1b467e607b31d
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-04-14 14:13:01 -0500

    upd

--- kayela-portfolio/src/routes/+layout.svelte
+++ kayela-portfolio/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.postcss';
-
+	import Header from "../components/Main.svelte";
+	import Footer from "../components/Main.svelte";
+	
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
@@ -20,6 +22,37 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	//window
+	let y;
+    let innerWidth = 0;
+    let innerHeight = 0;
+
+    function goTop() {
+        document.body.scrollIntoView();
+    }
+
 </script>
 
-<slot />
+<div
+    class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+>
+    <div
+        class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+            (y > 0
+                ? " opacity-full pointer-events-auto"
+                : " pointer-events-none opacity-0")}
+    >
+        <button
+            on:click={goTop}
+            class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+        >
+            <i class="fa-solid fa-arrow-up" />
+        </button>
+    </div>
+    <Header {y} {innerHeight}/>
+    <slot />
+    <Footer />
+</div>
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
+
